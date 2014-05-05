2aaa369c5948acb852f7037bf47a111f4c70b5e7
Anthony To <to.anthony93@gmail.com>
2014-05-05 16:24:06 -0500

    Make view controller, begin writing NSURLConnection

---
+++ Converge/ImagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesViewController.h
+//  Converge
+//
+//  Created by Anthony To on 5/5/14.
+//  Copyright (c) 2014 Vyshak Nagappala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesViewController : UIViewController
+
+@end
---
+++ Converge/ImagesViewController.m
@@ -0,0 +1,93 @@
+//
+//  ImagesViewController.m
+//  Converge
+//
+//  Created by Anthony To on 5/5/14.
+//  Copyright (c) 2014 Vyshak Nagappala. All rights reserved.
+//
+
+#import "ImagesViewController.h"
+
+@interface ImagesViewController (){
+  NSMutableData * responseData;
+}
+
+@end
+
+@implementation ImagesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+		// Open API call to get array of image urls
+		NSURL *url = [[NSURL alloc] initWithString:@"http://converge-rails.herokuapp.com/api/events/1/pictures"];
+		NSURLRequest *req = [[NSURLRequest alloc] initWithURL:url];
+		NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:req delegate:self];
+		[connection start];		
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark NSURLConnection Delegate Methods
+ 
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    // A response has been received, this is where we initialize the instance var you created
+    // so that we can append data to it in the didReceiveData method
+    // Furthermore, this method is called each time there is a redirect so reinitializing it
+    // also serves to clear it
+    responseData = [[NSMutableData alloc] init];
+}
+ 
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    // Append the new data to the instance variable you declared
+	[responseData appendData:data];
+}
+ 
+- (NSCachedURLResponse *)connection:(NSURLConnection *)connection
+                  willCacheResponse:(NSCachedURLResponse*)cachedResponse {
+    // Return nil to indicate not necessary to store a cached response for this connection 
+    return nil;
+}
+ 
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    // The request is complete and data has been received
+    // You can parse the stuff in your instance variable now
+	
+	  NSError *error = nil;
+	  NSDictionary *data = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:&error];
+		NSLog(data);
+	  [connection cancel];
+    
+}
+ 
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    // The request has failed for some reason!
+    // Check the error var
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
