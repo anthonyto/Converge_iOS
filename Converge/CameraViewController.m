f5da96e80e9a9e95b04f42bbba6b4223c6534070
Patron <patron@dyn-72-33-57-58.uwnet.wisc.edu>
2014-05-08 22:28:33 -0500

    Adding upload button on cameraview

--- Converge/CameraViewController.m
+++ Converge/CameraViewController.m
@@ -26,8 +26,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //self.view.backgroundColor = [UIColor colorWithRed:90/255.0 green:194/255.0 blue:215/255.0 alpha:1];
+    self.view.backgroundColor = [UIColor colorWithRed:0/255.0 green:0/255.0 blue:0/255.0 alpha:1];
     //NSLog(@"Event: %@", self.eventid);
 	// Do any additional setup after loading the view.
+    self.uploadButton.hidden = YES;
 }
 
 - (void) postImage {
@@ -40,6 +43,41 @@
     [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     [request setHTTPBody:data];
     NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];*/
+    // Dictionary that holds post parameters. You can set your post parameters that your server accepts or programmed to accept.
+    /*NSMutableDictionary* _params = [[NSMutableDictionary alloc] init];
+    [_params setObject:[NSString stringWithString:@"1.0"] forKey:[NSString stringWithString:@"ver"]];
+    [_params setObject:[NSString stringWithString:@"en"] forKey:[NSString stringWithString:@"lan"]];
+    [_params setObject:[NSString stringWithFormat:@"%d", userId] forKey:[NSString stringWithString:@"userId"]];
+    [_params setObject:[NSString stringWithFormat:@"%@",title] forKey:[NSString stringWithString:@"title"]];
+    
+    // the boundary string : a random string, that will not repeat in post data, to separate post data fields.
+    NSString *BoundaryConstant = [NSString stringWithString:@"----------V2ymHFg03ehbqgZCaKO6jy"];
+    
+    // string constant for the post parameter 'file'. My server uses this name: `file`. Your's may differ
+    NSString* FileParamConstant = [NSString stringWithString:@"file"];
+    
+    // the server url to which the image (or the media) is uploaded. Use your server url here
+    NSURL* requestURL = [NSURL URLWithString:@""];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    [request setHTTPShouldHandleCookies:NO];
+    [request setTimeoutInterval:30];
+    [request setHTTPMethod:@"POST"];
+    
+    // set Content-Type in HTTP header
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
+    [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    // post body
+    NSMutableData *body = [NSMutableData data];
+    
+    // add params (all params are strings)
+    for (NSString *param in _params) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]] dataUsingEncoding:NSUTF8StringEncoding]];
+    }*/
 }
 
 - (void)didReceiveMemoryWarning
@@ -64,6 +102,7 @@
     picker.delegate = self;
     picker.allowsEditing = YES;
     picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    //[picker.view addSubview:self.selectButton];
     
     [self presentViewController:picker animated:YES completion:NULL];
 }
@@ -71,9 +110,11 @@
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
     UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
     self.imageView.image = chosenImage;
     // I assume this is where the code would go but who the hell knows it's pretty hard to test.
-    self.imageView.image = [UIImage imageNamed:@"logo.png"]; // use an existing image for now to test post.
+    //self.imageView.image = [UIImage imageNamed:@"logo.png"]; // use an existing image for now to test post.
+    self.uploadButton.hidden = NO;
     [picker dismissViewControllerAnimated:YES completion:NULL];
     
 }
@@ -84,4 +125,7 @@
     
 }
 
+- (IBAction)uploadButton:(id)sender {
+    [self postImage];
+}
 @end
